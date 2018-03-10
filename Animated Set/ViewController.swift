526c35edead3452aad104cacc7b8a0dc37a5bd18
Oleg Gnashuk <oleg_gnashuk@i.ua>
2018-03-10 23:08:23 +0100

    Initial Commit

---
+++ Animated Set/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animated Set
+//
+//  Created by Oleg Gnashuk on 3/10/18.
+//  Copyright © 2018 Oleg Gnashuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
